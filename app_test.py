106a9c9614abb746b4839bc07b907bf2c1b45cb2
Valentin Khokhlov <hohlov.valentin@inbox.ru>
2025-04-23 16:42:23 +0700

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import datetime as dt
+from dateutil.relativedelta import relativedelta # to add days or years
 
 st.title('Тест простого веб-приложения на базе Streamlit')
 
@@ -7,7 +9,17 @@ name = st.text_input('Введите имя', '')
 
 st.write(f'Hello {name}!')
 
-x = st.slider('Выбери дату', '2025-04-01', 23, '2025-04-23')
+## Range selector
+cols1,_ = st.beta_columns((1,2)) # To make it narrower
+format = 'MMM DD, YYYY'  # format output
+start_date = dt.date(year=2021,month=1,day=1)-relativedelta(years=2)  #  I need some range in the past
+end_date = dt.datetime.now().date()-relativedelta(years=2)
+max_days = end_date-start_date
 
-# df = pd.DataFrame({'x': [x], 'y': [y] , 'x + y': [x + y]}, index = ['addition row'])
-st.write(x)
+slider = cols1.slider('Select date', min_value=start_date, value=end_date ,max_value=end_date, format=format)
+## Sanity check
+st.table(pd.DataFrame([[start_date, slider, end_date]],
+              columns=['start',
+                       'selected',
+                       'end'],
+              index=['date']))
