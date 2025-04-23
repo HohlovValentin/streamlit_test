998a332aaab9c805f76b2880268859d299011d2d
Valentin Khokhlov <hohlov.valentin@inbox.ru>
2025-04-23 16:46:08 +0700

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import datetime as dt
-from dateutil.relativedelta import relativedelta # to add days or years
 
 st.title('Тест простого веб-приложения на базе Streamlit')
 
@@ -13,7 +12,7 @@ st.write(f'Hello {name}!')
 cols1,_ = st.beta_columns((1,2)) # To make it narrower
 format = 'MMM DD, YYYY'  # format output
 start_date = dt.date(year=2021,month=1,day=1)-relativedelta(years=2)  #  I need some range in the past
-end_date = dt.datetime.now().date()-relativedelta(years=2)
+end_date = dt.datetime.now().date()
 max_days = end_date-start_date
 
 slider = cols1.slider('Select date', min_value=start_date, value=end_date ,max_value=end_date, format=format)
